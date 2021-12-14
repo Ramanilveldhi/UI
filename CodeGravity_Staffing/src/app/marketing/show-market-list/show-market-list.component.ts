89b5e9dca1fd7c0217ecfda48f73b0e1ec6ca497
CodegravityIt <96020008+CodegravityIt@users.noreply.github.com>
2021-12-15 01:50:57 +0530

    Dropdow selected changes

--- CodeGravity_Staffing/src/app/marketing/show-market-list/show-market-list.component.ts
+++ CodeGravity_Staffing/src/app/marketing/show-market-list/show-market-list.component.ts
@@ -11,11 +11,20 @@ export class ShowMarketListComponent implements OnInit {
 
   constructor(private service :SharedService) { };
   marketinglist:any=[];
- // empllist:any[];
+   empList:any=[];
+   tempEmplist:any=[];
+   data:Array<Object> = [
+    {Emp_Id: 0, Emp_Name: "Select"}
+];
+selectedLevel:any;
 
+selected(){
+  alert(this.selectedLevel.name)
+}
   ngOnInit(): void {
     this.refreshmarketingList();
-    //this.getemployeename();
+    this.getemployeename();
+    
   }
 
   refreshmarketingList(){
@@ -24,10 +33,20 @@ export class ShowMarketListComponent implements OnInit {
     });
   }
 
-  // getemployeename(){
-  //   this.service.getmarketinglist().subscribe((val: any)=>{
-  //     this.empllist=val;
-  //   });
-  // }
+  getemployeename(){
+    this.service.getemployeelist().subscribe((val: any)=>{
+      this.empList=val;
+      this.data=this.empList;
+    });
+  }
+  onOptionsSelected(id:any){   
+    
+    this.data = this.data.filter((abc:any)=>abc.Emp_Id==id
+
+    )
+
+  }
+
+
 
 }
