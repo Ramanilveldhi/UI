e7b7a90c78b22f359f4912af47bcd05498fbec0f
CodegravityIt <96020008+CodegravityIt@users.noreply.github.com>
2021-12-15 13:13:44 +0530

    add employee details

--- CodeGravity_Staffing/src/app/app-routing.module.ts
+++ CodeGravity_Staffing/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {ConsultantComponent} from './consultant/consultant.component';
 import {IncentiveComponent} from './incentive/incentive.component';
 import {MarketingComponent} from './marketing/marketing.component';
 import { PurchaseorderComponent } from './purchaseorder/purchaseorder.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import { AddEditConsulComponent } from './consultant/add-edit-consul/add-edit-consul.component';
+import { AddEditMarketComponent } from './marketing/add-edit-market/add-edit-market.component';
+
 
 const routes: Routes = [
   {path:'employee',component:EmployeeComponent},
@@ -12,6 +16,9 @@ const routes: Routes = [
   {path:'incentive',component:IncentiveComponent},
   {path:'market',component:MarketingComponent},
   {path:'purchaseorder',component:PurchaseorderComponent},
+  {path:'addemployee',component:AddEditEmpComponent},
+  {path:'addconsult',component:AddEditConsulComponent},
+  {path:'addMarketing',component:AddEditMarketComponent}
 
 ];
 
--- CodeGravity_Staffing/src/app/employee/add-edit-emp/add-edit-emp.component.ts
+++ CodeGravity_Staffing/src/app/employee/add-edit-emp/add-edit-emp.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class AddEditEmpComponent implements OnInit {
 
   constructor() { }
-  @Input() dep:any;
+  @Input() emp:any;
 
   ngOnInit(): void {
   }
