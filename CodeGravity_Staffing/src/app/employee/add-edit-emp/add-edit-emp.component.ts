43643bdfda73518df98e8e8e13be5a5c1e2fe2d8
CodegravityIt <96020008+CodegravityIt@users.noreply.github.com>
2021-12-18 21:06:41 +0530

    added country list

--- CodeGravity_Staffing/src/app/employee/add-edit-emp/add-edit-emp.component.ts
+++ CodeGravity_Staffing/src/app/employee/add-edit-emp/add-edit-emp.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {addEmployee} from './add-edit-emp.model';
-import {SharedService} from 'src/app/shared.service';
+import { addEmployee } from './add-edit-emp.model';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-emp',
@@ -8,16 +8,45 @@ import {SharedService} from 'src/app/shared.service';
   styleUrls: ['./add-edit-emp.component.css']
 })
 export class AddEditEmpComponent implements OnInit {
-  addEmployee=new addEmployee();
-  constructor( private service :SharedService){ }
-  @Input() emp:any;
+  addEmployee = new addEmployee();
+  constructor(private service: SharedService) { }
+  @Input() emp: any;
 
+  countrylist: any = [];
+  entitlementlist: any = [];
+  incentivetypelist: any = [];
+
+  selectedValue:any;
   ngOnInit(): void {
+    this.getCountryList();
+    this.getEntitlementList();
+    this.getIncentivetypeList();
   }
-  submitData(){
-    this.service.addemployee(addEmployee).subscribe(res=>{console.log(res);
+  submitData() {
+    this.service.addemployee(addEmployee).subscribe(res => {
+      console.log(res);
     })
     //console.log(this.addEmployee);
+  }
+  getCountryList() {
+    this.service.getcountrytlist().subscribe((val: any) => {
+      this.countrylist = val;
+    });
+  }
+  getEntitlementList() {
+    this.service.getentitlementlist().subscribe((val: any) => {
+      this.entitlementlist = val;
+    });
+  }
+  getIncentivetypeList() {
+    this.service.getincentivetypetlist().subscribe((val: any) => {
+      this.incentivetypelist = val;
+    });
+  }
+  onCountryChange(item: any) {
+    // const value = item.target.value;
+    
+
   }
 
 }
