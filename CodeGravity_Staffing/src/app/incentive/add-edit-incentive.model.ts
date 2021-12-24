67b03ef06508ae38316f8fabe7ff9c0b468b79fd
CodegravityIt <96020008+CodegravityIt@users.noreply.github.com>
2021-12-25 01:14:08 +0530

    added new changes

---
+++ CodeGravity_Staffing/src/app/incentive/add-edit-incentive.model.ts
@@ -0,0 +1,31 @@
+export class addincentive {
+    Id:any;
+    Consult_id: any;
+    Placed_Sales_Recruiter: any;
+    Placed_Tech: any;
+    PO_Date: any;
+    Visa_Type: any;
+    Project_Start_Date: any;
+    Project_Duration: any;
+    Project_End_Date: any;
+    Vendor_Name: any;
+    Vendor_SPOC_Name: any;
+    Vendor_SPOC_Email: any;
+    Vendor_SPOC_PhoneNumber: any;
+    Vendor_Address: any;
+    Client_Name: any;
+    Client_SPOC_Name: any;
+    Client_SPOC_Email: any;
+    Client_SPOC_PhoneNumber: any;
+    Client_Address: any;
+    Project_Type: any;
+    Bill_Rate: any;
+    Consultant_Pay_Rate: any;
+    Notes: any;
+    Placement_Status: any;
+    Created_Date: any;
+    Created_by: any;
+    Modified_Date: any;
+    Modified_by: any;
+    Consult_Name:any;
+  }
